cdfe6bdd23410c6f10e8cc5ba72b7ec17283860d
Joe <jakatsa5@gmail.com>
2024-02-18 12:07:50 +0300

    updates

--- OnlyBeats/src/firebase/firebase-config.js
+++ OnlyBeats/src/firebase/firebase-config.js
@@ -2,10 +2,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBER5B6VXW3wVYqvIJERFwWuoLN0cuBGLA",
   authDomain: "onlybeatsss-3f220.firebaseapp.com",
@@ -15,6 +14,6 @@ const firebaseConfig = {
   appId: "1:103852255104:web:e4fbcdaaee77bb7dfa8365",
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
